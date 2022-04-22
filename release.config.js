0ea1a8ff2d81c0617fb49505b5ec5f35699def00
Spyros Argalias <sargalias@gmail.com>
2022-04-23 00:36:45 +0100

    feat: Add workflows

---
+++ release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: "main",
+  repositoryUrl: "https://github.com/sargalias/github-actions-course-tccawg-2",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
+};
